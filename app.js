e76be20d6bbc19739aa19ad3434c91f8c320459e
Lawrence <lawrencew711kb@gmail.com>
2023-06-21 17:52:50 +0800

    fix bug

--- app.js
+++ app.js
@@ -1,92 +1,92 @@
-require('dotenv').config();
-const express = require('express');
-const line = require('@line/bot-sdk');
-
-const config = {
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-    channelSecret: process.env.CHANNEL_SECRET
-};
-
-const client = new line.Client(config)
-
-
-const app = express();
-
-app.post('/callback',line.middleware(config),(req, res)=> {
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result))
-        .catch((err) => {
-            console.error(err);
-            res.status(500).end();
-
-        });
-});
-
-function handleEvent(event) {
-    if(event.type !== 'message' || event.type !== 'text'){
-        return Promise.resolve(null);
-    }
-    const echo = {type: 'text', text: event.message.text}
-    return client.replyMessage(event.replyToken, echo);
-
-}
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`listening on ${port}`);
-});
-
-
-
-
-
 // require('dotenv').config();
-
-// const line = require('@line/bot-sdk');
 // const express = require('express');
+// const line = require('@line/bot-sdk');
 
-// // create LINE SDK config from env variables
 // const config = {
-//   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-//   channelSecret: process.env.CHANNEL_SECRET,
+//     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+//     channelSecret: process.env.CHANNEL_SECRET
 // };
 
-// // create LINE SDK client
-// const client = new line.Client(config);
+// const client = new line.Client(config)
 
-// // create Express app
-// // about Express itself: https://expressjs.com/
+
 // const app = express();
 
-// // register a webhook handler with middleware
-// // about the middleware, please refer to doc
-// app.post('/callback', line.middleware(config), (req, res) => {
-//   Promise
-//     .all(req.body.events.map(handleEvent))
-//     .then((result) => res.json(result))
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500).end();
-//     });
+// app.post('/callback',line.middleware(config),(req, res)=> {
+//     Promise
+//         .all(req.body.events.map(handleEvent))
+//         .then((result) => res.json(result))
+//         .catch((err) => {
+//             console.error(err);
+//             res.status(500).end();
+
+//         });
 // });
 
-// // event handler
 // function handleEvent(event) {
-//   if (event.type !== 'message' || event.message.type !== 'text') {
-//     // ignore non-text-message event
-//     return Promise.resolve(null);
-//   }
+//     if(event.type !== 'message' || event.type !== 'text'){
+//         return Promise.resolve(null);
+//     }
+//     const echo = {type: 'text', text: event.message.text}
+//     return client.replyMessage(event.replyToken, echo);
 
-//   // create a echoing text message
-//   const echo = { type: 'text', text: event.message.text };
-
-//   // use reply API
-//   return client.replyMessage(event.replyToken, echo);
 // }
 
-// // listen on port
 // const port = process.env.PORT || 3000;
 // app.listen(port, () => {
-//   console.log(`listening on ${port}`);
+//     console.log(`listening on ${port}`);
 // });
+
+
+
+
+
+require('dotenv').config();
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
